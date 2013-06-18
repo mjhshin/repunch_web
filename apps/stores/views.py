1261b9683a3006c2c1dac136a2959e5160b83e45
vestrel00 <vestrel00@gmail.com>
2013-06-18 06:41:09 -0400

    Completed apps/stores/views caching.

--- apps/stores/views.py
+++ apps/stores/views.py
@@ -11,6 +11,7 @@ from apps.stores.models import Store as dStore, Hours as dHours
 from apps.stores.forms import StoreForm, StoreAvatarForm
 from libs.repunch.rphours_util import HoursInterpreter
 
+from parse import session as SESSION
 from parse.utils import delete_file, create_png, cloud_call
 from parse.apps.stores.models import Store
 from parse.auth.decorators import login_required
@@ -18,7 +19,7 @@ from parse.auth.decorators import login_required
 @login_required
 def punch(request):
     if request.method == "POST" or request.is_ajax():
-        store = request.session['account'].get('store')
+        store = SESSION.get_store(request.session)
         data = {
             "store_id":store.objectId,
             "store_name":str(store.get('store_name')),
@@ -46,7 +47,7 @@ def index(request):
 @login_required
 def edit(request):
     data = {'account_nav': True}
-    account = request.session['account']
+    store = SESSION.get_store(request.session)
     # fake a store to construct HoursFormset - probably not necessary
     dstore_inst = dStore()
             
@@ -56,9 +57,8 @@ def edit(request):
         formset = HoursFormSet(request.POST, prefix='hours',
                                 instance=dstore_inst) 
         form = StoreForm(request.POST)
-        print form.is_valid()
         if form.is_valid(): 
-            store = Store(**account.get("store").__dict__)
+            store = Store(**store.__dict__)
             store.update_locally(request.POST.dict(), False)
             
             # build the list of hours in proper format for saving 
@@ -66,7 +66,8 @@ def edit(request):
             hours, ind, key = [], 0, "hours-0-days"
             while ind < 7:
                 days = request.POST.getlist(key)
-                if days:
+                if days and not request.POST.get('hours-' +\
+                        str(ind) + '-DELETE'):
                     # format time from 10:00:00 to 1000
                     open_time = request.POST["hours-" + str(ind) +\
                                 "-open"].replace(":", "").zfill(6)[:4]
@@ -84,37 +85,38 @@ def edit(request):
             store.set("hours", hours)
             store.update()
 
-            account.store = store
-            request.session['account'] = account
             data['success'] = "Store details have been saved."
     else:
         form = StoreForm()
-        form.initial = account.get("store").__dict__
-        form.data['email'] = account.get('email')
-        hours_map = {}
-        # group up the days that aare in the same row
-        # also need to shift days frm 0-6 to 1-7
-        if account.get('store').get("hours"):
-            for hour in account.get('store').get("hours"):
-                key = (hour['close_time'], hour['open_time'])
-                if key in hours_map:
-                    hours_map[key].append(unicode(hour['day']+1))
-                else:
-                    hours_map[key] = [unicode(hour['day']+1)]
-                
-        # create the formset
-        HoursFormSet = inlineformset_factory(dStore, dHours,
-                            max_num=7, extra=len(hours_map))
-        formset = HoursFormSet(prefix='hours', instance=dstore_inst)
-        # set forms in formset initial data
-        for i, key in enumerate(hours_map.iterkeys()):
-            data = {'days':hours_map[key],
-                    'open':unicode(key[1][:2] +\
-                            ':' + key[1][2:4] + ':00'),
-                    'close':unicode(key[0][:2] +\
-                            ':' + key[0][2:4] + ':00'),
-                    'list_order':unicode(i+1) }
-            formset[i].initial = data
+        form.initial = store.__dict__
+        
+    hours_map = {}
+    # group up the days that aare in the same row
+    # also need to shift days frm 0-6 to 1-7
+    if store.get("hours"):
+        for hour in store.get("hours"):
+            key = (hour['close_time'], hour['open_time'])
+            if key in hours_map:
+                hours_map[key].append(unicode(hour['day']+1))
+            else:
+                hours_map[key] = [unicode(hour['day']+1)]
+            
+    # create the formset
+    HoursFormSet = inlineformset_factory(dStore, dHours,
+                        max_num=7, extra=len(hours_map))
+    formset = HoursFormSet(prefix='hours', instance=dstore_inst)
+    # set forms in formset initial data
+    for i, key in enumerate(hours_map.iterkeys()):
+        d = {'days':hours_map[key],
+                'open':unicode(key[1][:2] +\
+                        ':' + key[1][2:4] + ':00'),
+                'close':unicode(key[0][:2] +\
+                        ':' + key[0][2:4] + ':00'),
+                'list_order':unicode(i+1) }
+        formset[i].initial = d
+            
+    # update the session cache
+    request.session['store'] = store
        
     data['form'] = form
     data['hours_formset'] = formset
@@ -123,8 +125,6 @@ def edit(request):
 
 @login_required
 def hours_preview(request):
-    store = request.session['account'].get('store')
-    
     HoursFormSet = inlineformset_factory(dStore, dHours, extra=0)
     
     hours = []
@@ -142,8 +142,7 @@ def hours_preview(request):
 @login_required
 def avatar(request):
     data = {}
-    account = request.session['account']
-    store = account.get('store')
+    store = SESSION.get_store(request.session)
     
     if request.method == 'POST': 
         form = StoreAvatarForm(request.POST, request.FILES)
@@ -157,12 +156,13 @@ def avatar(request):
                 store.store_avatar = res.get('name')
                 store.store_avatar_url = res.get('url')
             store.update()
-            account.store = store
-            request.session['account'] = account;
             
             data['success'] = True
     else:
         form = StoreAvatarForm();
+    
+    # update the session cache
+    request.session['store'] = store
     
     data['form'] = form
     data['url'] = reverse('store_avatar')
