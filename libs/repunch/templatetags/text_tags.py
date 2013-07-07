2aa4720ec741320ee9657bc29db3487548c01803
vestrel00 <vestrel00@gmail.com>
2013-07-07 00:40:18 -0400

    Overflow text now trimmed serverside.

---
+++ libs/repunch/templatetags/text_tags.py
@@ -0,0 +1,16 @@
+from django import template
+
+register = template.Library()
+
+@register.filter
+def trim_to_dots(string, x):
+    """
+    Returns the first x chars of the string and truncates the rest.
+    Appends 3 dots at the end.
+
+    e.g. "hello world|trim_to_dots:5 returns "hello..."
+    """
+    if len(string) > x:
+        return string[:x] + "..."
+    else:
+        return string
