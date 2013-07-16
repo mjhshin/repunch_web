c06a81d746c9e9eaec1dabc323c87be924e6dbc3
vestrel00 <vestrel00@gmail.com>
2013-07-16 13:21:41 -0400

    Paginator truncation 100%

--- static/manage/js/paginator.js
+++ static/manage/js/paginator.js
@@ -3,6 +3,7 @@
 */
 
 var paginate, getPage, onHeaderClick, onTabClick;
+var MAX_PAG_UNIT_COUNT = 11;
 
 /*
     getCallback is called after every successful ajax call to the
@@ -70,9 +71,33 @@ paginate = function (pagUrl, which, getCallback) {
     if (pagCount == 1){ return; }
     
     // add the pag-units 
-    for (var i=0; i<pagCount; i++){
-        pagContainer.append("<a class='pag-unit'>" + new String(i+1) + "</a>");
-    } 
+    if (pagCount > MAX_PAG_UNIT_COUNT) {
+        var pagePageNum = new Number(pagPage.val());
+        // show up to 4 to the left and 4 to the right
+        var numLeft = pagePageNum - parseInt(MAX_PAG_UNIT_COUNT/2);
+        if (numLeft <= 0) { numLeft = 1; }
+        var numRight = pagePageNum + parseInt(MAX_PAG_UNIT_COUNT/2);
+        if (numRight > pagCount) { numRight = pagCount; }
+        
+        var diff = numRight - numLeft;
+        if (diff < MAX_PAG_UNIT_COUNT - 2) { // subtract 2 for first and last
+            if (numLeft == 1) { // left side is maxed
+                numRight = numRight + ((MAX_PAG_UNIT_COUNT - 2) - diff);
+            } else if (numRight == pagCount){ // right is maxed
+                numLeft = numLeft - ((MAX_PAG_UNIT_COUNT - 2) - diff);
+            }
+        }
+        
+        pagContainer.append("<a class='pag-unit'>first</a>");
+        for (var i=numLeft; i<numRight; i++){
+            pagContainer.append("<a class='pag-unit'>" + new String(i+1) + "</a>");
+        }   
+        pagContainer.append("<a class='pag-unit'>last</a>");
+    } else {
+        for (var i=0; i<pagCount; i++){
+            pagContainer.append("<a class='pag-unit'>" + new String(i+1) + "</a>");
+        } 
+    }
     // activate the current page pag unit
     $("#pag-container a.pag-unit:nth-child(" + new String(pagPage.val()) + ")").addClass("active");
     // now bind the units
@@ -88,12 +113,89 @@ paginate = function (pagUrl, which, getCallback) {
         } else {
             order = "asc";
         }
-        getPage(pagUrl, self.text(), type, order, header.attr("id").substring("header-".length), getCallback); 
-        // remove all siblings' active class
-        self.siblings().removeClass("active");
-        self.addClass("active");
-        // set the current page
-        pagPage.val(self.text());
+        
+        var pageNumStr;
+        if (self.text() == "first") {
+            pageNumStr = "1";
+        } else if(self.text() == "last") {
+            pageNumStr = pagCount;
+        } else {
+            pageNumStr = self.text();
+        }
+        
+        getPage(pagUrl, pageNumStr, type, order, header.attr("id").substring("header-".length), getCallback); 
+        
+        // renumber the units if passed the threshold;
+        if (pagCount > MAX_PAG_UNIT_COUNT) {
+            var pagePageNum;  
+            if (self.text() == "first") {
+                pagePageNum = 1;
+            } else if (self.text() == "last") {
+                pagePageNum = pagCount;
+            } else {
+                pagePageNum = new Number(self.text());
+            }
+            // show up to 4 to the left and 4 to the right
+            var numLeft = pagePageNum - parseInt(MAX_PAG_UNIT_COUNT/2);
+            var numRight = pagePageNum + parseInt(MAX_PAG_UNIT_COUNT/2);
+            // truncate
+            if (numLeft <= 1) { 
+                numLeft = 1;
+            }
+            
+            var diff = numRight - numLeft;
+            if (diff < MAX_PAG_UNIT_COUNT - 2) { // subtract 2 for first and last
+                if (numLeft <= 1) { // left side is maxed
+                    numRight = numRight + ((MAX_PAG_UNIT_COUNT - 2) - diff);
+                } else if (numRight >= pagCount){ // right is maxed
+                    numLeft = numLeft - ((MAX_PAG_UNIT_COUNT - 2) - diff);
+                }
+            } else if (diff > MAX_PAG_UNIT_COUNT - 2) {
+                numRight = numRight - (diff - (MAX_PAG_UNIT_COUNT - 2));
+                numLeft = numLeft - (diff - (MAX_PAG_UNIT_COUNT - 2) - 1);
+            } else {
+                if (numRight >= pagCount){ // right is maxed
+                    numLeft = numLeft - 1;
+                }
+            }
+            // truncate again
+            if (numRight >= pagCount) { 
+                numRight = pagCount - 1;
+                numLeft = pagCount - (MAX_PAG_UNIT_COUNT  - 2) - 1;
+            }
+            if (numLeft <= 1) { 
+                numLeft = 1;
+            }
+            alert("left: " + numLeft + ", right: " + numRight + ", pagCount: " + pagCount + ", diff: " + diff);
+            
+            var countTmp = numLeft - 1;
+            $(".pag-unit").each(function() {
+                var self = $(this);
+                if (countTmp == numLeft - 1) {
+                    self.text("first");
+                } else if (countTmp == numRight) {
+                    self.text("last");
+                } else {
+                    self.text(new String(countTmp + 1));
+                }
+                if (new Number(pageNumStr) - 1 == countTmp) {
+                    // remove all siblings' active class
+                    self.siblings().removeClass("active");
+                    self.addClass("active");
+                    // set the current active unit
+                    pagPage.val(self.text());
+                }
+                countTmp = countTmp + 1;
+            });
+            
+        } else {
+            // remove all siblings' active class
+            self.siblings().removeClass("active");
+            self.addClass("active");
+            // set the current active unit
+            pagPage.val(self.text());
+        }
+        
     });
     
 }
