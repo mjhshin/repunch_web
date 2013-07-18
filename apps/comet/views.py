878dabd60daed8984494e1c63016f35233b822a8
vestrel00 <vestrel00@gmail.com>
2013-07-18 11:38:29 -0400

    Fixed avatar upload dialog overflow.

--- apps/comet/views.py
+++ apps/comet/views.py
@@ -455,6 +455,7 @@ def refresh(request):
         try: # attempt to get a used CometSession first
             scomet = CometSession.objects.get(session_key=\
                 request.session.session_key)
+            print str(timezone.now()) + ": Request idling"
             if scomet.modified:
                 scomet.modified = False
                 scomet.save()
