fd10d7ed1eab9f1fd66363c93fed6679c9e06cd6
vestrel00 <vestrel00@gmail.com>
2013-07-20 19:51:44 -0400

    setup progress for user pass limit script.

--- apps/scripts/management/commands/passed_user_limit.py
+++ apps/scripts/management/commands/passed_user_limit.py
@@ -13,11 +13,44 @@ Account disabled: 14th day from now
 
 
 from django.utils import timezone
+from django.core import mail
 from django.core.management.base import BaseCommand
 
-from parse.notifications import 
+from libs.dateutil.relativedelta import relativedelta
+from parse.notifications import send_email_passed_user_limit
 from parse.apps.accounts import sub_type
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
-        pass
+        conn = mail.get_connection(fail_silently=(not DEBUG))
+        conn.open()
+        now = timezone.now()
+        # 1st day time range
+        day1_end = now.replace()
+        day1_start = day1_end + relativedelta(hours=-24)
+        # 4th day time range
+        day4_end = now + relativedelta(days=-4)
+        day4_start = day4_end + relativedelta(hours=-24)
+        # 8th day time range
+        day8_end = now + relativedelta(days=-8)
+        day8_start = day8_end + relativedelta(hours=-24)
+        # 14th day time range
+        day14_end = now + relativedelta(days=-14)
+        day14_start = day14_end + relativedelta(hours=-24)
+        
+        ## SUB_TYPE 0
+        # 1st day
+        # 4th day
+        # 8th day
+        # 14th day
+        
+        
+        ## SUB_TYPE 1
+        # 1st day
+        # 4th day
+        # 8th day
+        # 14th day
+        
+        
+        
+        conn.close()
