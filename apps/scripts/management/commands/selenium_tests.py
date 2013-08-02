cf47debe740482dd4542375a3b0d8bdecbfa4054
vestrel00 <vestrel00@gmail.com>
2013-08-02 12:41:17 -0400

    wrote tests for dedicated login page. All public pages are now tested. Moving to dashboard testing. =)

--- apps/scripts/management/commands/selenium_tests.py
+++ apps/scripts/management/commands/selenium_tests.py
@@ -6,7 +6,7 @@ from django.core.management.base import BaseCommand
 
 from tests.test_public_pages import test_public_pages
 from tests.test_signup import test_signup
-from tests.test_login_logout import test_login_dialog#, test_login_page
+from tests.test_login_logout import test_login_dialog, test_login_page
 from parse.notifications import send_email_selenium_test_results
 
 class Command(BaseCommand):
@@ -17,7 +17,9 @@ class Command(BaseCommand):
         # results.extend(test_public_pages())
         # SIGNUP
         # results.extend(test_signup())
-        # LOGIN
-        results.extend(test_login_dialog())
+        # LOGIN (dialog)
+        # results.extend(test_login_dialog())
+        # LOGIN (dedicated page)
+        results.extend(test_login_page())
         
         send_email_selenium_test_results(results)
--- tests/test_login_logout.py
+++ tests/test_login_logout.py
@@ -136,3 +136,124 @@ def test_login_dialog():
     
     # END OF ALL TESTS - cleanup
     return test.tear_down()
+
+def test_login_page():
+    """
+    The dedicated login page.
+    """
+    test = SeleniumTest()
+    parts = [ 
+        {'test_name': "Wrong login credentials show error"},
+        {'test_name': "Successful login redirects to dashboard"},
+        {'test_name': "Not having the stay signed in option "+\
+            "sets the sessionid's cookie expiry to None"},
+        {'test_name': "Having the stay signed in option "+\
+            "sets the sessionid's cookie expiry to a number"},
+        {'test_name': "Logout works"},
+        {'test_name': "Forgot password form functional"},
+    ]
+    section = {
+        "section_name": "Login page working properly?",
+        "parts": parts,
+    }
+    test.results.append(section)
+    
+    test.open(reverse("manage_login")) # ACTION!
+    
+    ##########  Wrong login credentials show error
+    selectors = (
+        ("#id_username", TEST_USER['username']),
+        ("#id_password", TEST_USER['password'] + "sdg"),
+        ("", Keys.RETURN)
+    )
+    try:
+        test.action_chain(1, selectors, "send_keys") # ACTION!
+        sleep(3)
+        parts[0]["success"] =\
+            str(test.find("#dialog-login-message").text) ==\
+                "The username or password you entered is incorrect."
+    except Exception as e:
+        print e
+        
+    # clear the input fields
+    test.find("#id_username").clear()
+    test.find("#id_password").clear()
+        
+    ##########  Successful login redirects to dashboard
+    selectors = (
+        ("#id_username", TEST_USER['username']),
+        ("#id_password", TEST_USER['password']),
+        ("", Keys.RETURN)
+    )
+    try:
+        test.action_chain(1, selectors, "send_keys") # ACTION!
+        sleep(7)
+        parts[1]["success"] =\
+            test.is_current_url(reverse("store_index"))
+    except Exception as e:
+        print e
+    
+    ##########  Not having the stay signed in option
+    ##########  sets the sessionid's cookie expiry to None
+    parts[2]["success"] = test.driver.get_cookie(\
+        "sessionid")['expiry'] == None
+    
+    ##########  Having the stay signed in option
+    ##########  sets the sessionid's cookie expiry to a number
+    selectors = (
+        ("#id_username", TEST_USER['username']),
+        ("#id_password", TEST_USER['password']),
+        ("", Keys.RETURN)
+    )
+    try:
+        test.new_driver(save_session_cookie=False)
+        test.open(reverse("manage_login")) # ACTION!
+        sleep(1)
+        test.find("#stay_in").click() # ACTION!
+        test.action_chain(1, selectors, "send_keys") # ACTION!
+        sleep(7)
+        parts[3]["success"] = test.driver.get_cookie(\
+            "sessionid")['expiry'] != None
+    except Exception as e:
+        print e
+    
+    ##########  Logout works
+    try:
+        test.find("#link-logout").click() # ACTION!
+        sleep(2)
+        parts[4]["success"] =\
+            test.is_current_url(reverse("public_home"))
+    except Exception as e:
+        print e
+    
+    ##########  Forgot password form functional
+    try:
+        test.open(reverse("manage_login"))
+        sleep(1)
+        test.find("//form[@id='forgot-pass-form']/a",
+            type="xpath").click() # ACTION!
+        sleep(3)
+        test.find("//div[@id='forgot-pass-form-div']/input[" +\
+            "@name='forgot-pass-email']", type="xpath").send_keys(\
+                TEST_USER['email'])
+        sleep(1)
+        test.find("//div[@id='forgot-pass-form-div']/input[" +\
+            "@type='submit']", type="xpath").click()
+        sleep(3)
+        parts[5]['success'] = str(test.find(\
+            "#forgot-pass-form").text) == "Password Reset form sent."
+    except Exception as e:
+        print e
+    
+    # END OF ALL TESTS - cleanup
+    return test.tear_down()
+
+
+
+
+
+
+
+
+
+
