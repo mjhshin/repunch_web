c34ca9b4645842a9ee04b5947cd365e43e3624cd
mjhshin <mike@repunch.com>
2013-09-20 13:13:34 -0400

    set punch code email column

--- cloud_code/production/cloud/main.js
+++ cloud_code/production/cloud/main.js
@@ -25,7 +25,7 @@ Parse.Cloud.define("register_patron", function(request, response) {
 	punchCodeQuery.first().then(function(punchCode) {
 		console.log("PunchCode fetch success.");
         punchCode.set("is_taken", true);
-        punchCode.set("username", username);
+        punchCode.set("email", email);
 		return punchCode.save();
 			
 	}, function(error) {
