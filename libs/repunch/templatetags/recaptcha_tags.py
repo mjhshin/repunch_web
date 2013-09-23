5a73744907b8226d2948376652a9b78920186066
vestrel00 <vestrel00@gmail.com>
2013-09-23 11:45:38 -0400

    fixed recaptcha displaying after 1 fail attempt then refreshing page

--- libs/repunch/templatetags/recaptcha_tags.py
+++ libs/repunch/templatetags/recaptcha_tags.py
@@ -1,10 +1,18 @@
 from django import template
 
 from libs import recaptcha
+from repunch.settings import RECAPTCHA_TOKEN, RECAPTCHA_ATTEMPTS
 
 register = template.Library()
 
 @register.filter
-def display_recaptcha(x):
+def display_recaptcha(session):
+    """ returns true if recaptcha should be displayed """
+    return RECAPTCHA_TOKEN in session and session[\
+            RECAPTCHA_TOKEN] >= RECAPTCHA_ATTEMPTS
+    
+    
+@register.filter
+def get_recaptcha(x):
     """ calls recaptcha displayhtml """
     return recaptcha.displayhtml()
