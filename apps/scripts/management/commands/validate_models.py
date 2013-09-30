2f7ab604b41e40c1e3c788784dc577a6fe085a00
vestrel00 <vestrel00@gmail.com>
2013-09-30 11:48:22 -0400

    imported all models in validate_models

--- apps/scripts/management/commands/validate_models.py
+++ apps/scripts/management/commands/validate_models.py
@@ -2,21 +2,26 @@
 Queries all Parse models/classes for invalid columns.
 --------------------------------------------------------
 
-# Check for null columns
+# Check for null columns & other conditions
 --------------------------
 Uses ParseObject.fields_required to get the list of Parse columns
 that cannot be null. A parse query is made for each element in the 
-list to see if it is null. The object(s) that have null columns,
-which are not supposed to be null are then added to NULL_COLUMNS.
-
+list to see if it is null or does not meet a specific condition.
+See ParseObject.fields_required for full documentation.
 """
 
 from django.core.management.base import BaseCommand
 
+from parse.apps.accounts.models import Account
+from parse.apps.employees.models import Employee
+from parse.apps.messages.models import Message, MessageStatus
+from parse.apps.rewards.models import Punch, RedeemReward
+from parse.apps.stores.models import Store, Settings, Subscription, Invoice
+from parse.apps.patrons.models import Patron, PatronStore, PunchCode, FacebookPost
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
-        pass    
+        pass
     
     
     
