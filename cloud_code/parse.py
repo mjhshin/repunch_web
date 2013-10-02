8df187fc800377c984db97f90e8746c28ea5a8ff
vestrel00 <vestrel00@gmail.com>
2013-10-02 15:21:45 -0400

    fixed parse.py

--- cloud_code/parse.py
+++ cloud_code/parse.py
@@ -7,7 +7,7 @@ between production and development cloud codes.
 All edits to the production and development cloud codes are ignored!
 """
 
-import os, subprocess, shlex, re, argparse
+import os, subprocess, shlex, argparse
 
 DEVELOPMENT_COMET_RECEIVE_URL = "http://dev.repunch.com/manage/comet/receive/"
 DEVELOPMENT_COMET_RECEIVE_KEY = "384ncocoacxpvgrwecwy"
@@ -19,7 +19,6 @@ COMET_RECEIVE_URL_DELIMETER = "<<COMET_RECEIVE_URL>>"
 COMET_RECEIVE_KEY_DELIMETER = "<<COMET_RECEIVE_KEY>>"
 
 SCRIPT_DESCRIPTION = "Uses the master cloud code to deploy to the Production or Development Server."
-SLAVE_TEXT = "All changes to this file are ignored! Edit the master code instead."
 
 COMMAND_DEPLOY = "deploy"
 TARGET_DEV = "dev"
@@ -37,30 +36,38 @@ command, target, option = args.command, args.target, args.option
 if command == COMMAND_DEPLOY:
     # open the master cloud code and the corresponding target cloud code
     if target == TARGET_DEV:
-        target_file = "./development/"
+        target_dir = "./development/"
         comet_receive_url = DEVELOPMENT_COMET_RECEIVE_URL
         comet_receive_key = DEVELOPMENT_COMET_RECEIVE_KEY
         
     elif target == TARGET_PROD:
-        target_file = "./production/"
+        target_dir = "./production/"
         comet_receive_url = PRODUCTION_COMET_RECEIVE_URL
         comet_receive_key = PRODUCTION_COMET_RECEIVE_KEY
         
-    target_file += "cloud/main.js"
+    target_file = target_dir + "cloud/main.js"
     
-    with open("./master/cloud/main.js", "r") as master,
+    with open("./master/cloud/main.js", "r") as master,\
         open(target_file, "w+") as slave:
         print "Reading the master code..."
-        # TODO
-        print "Replacing COMET_RECEIVE_URL..."
-        # TODO
-        print "Replacing COMET_RECEIVE_KEY..."
-        # TODO
-        print "Writing to %s" % (target_file, )
-        # TODO
-        print "Deploying to %s" % (target, )
-        # TODO
+        master_code = master.read()
         
+        print "Replacing COMET_RECEIVE_URL..."
+        master_code = master_code.replace(COMET_RECEIVE_URL_DELIMETER, comet_receive_url)
+        
+        print "Replacing COMET_RECEIVE_KEY..."
+        master_code = master_code.replace(COMET_RECEIVE_KEY_DELIMETER, comet_receive_key)
+        
+        print "Writing to %s..." % (target_file, )
+        slave.write(master_code)
+        
+    # this must be called outside the with block to prevent io conflicts
+    print "Deploying to %s..." % (target, )
+    os.chdir(target_dir)
+    if option:
+        subprocess.call(shlex.split("parse deploy " + option))
+    else:
+        subprocess.call(shlex.split("parse deploy"))
         
         
         
