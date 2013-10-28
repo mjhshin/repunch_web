9282222379b2f78ef7859a5698756c2aa7cb5624
vestrel00 <vestrel00@gmail.com>
2013-10-28 12:25:07 -0400

    gcm_send now batching posts to gcm servers.

--- libs/gcm/__init__.py
+++ libs/gcm/__init__.py
@@ -7,8 +7,10 @@ import requests, json
 from repunch.settings import GCM_RECEIVE_KEY, GCM_RECEIVE_KEY_NAME,\
 GCM_API_KEY
 
-NON_DATA_PARAMS = (GCM_RECEIVE_KEY_NAME, 'registration_ids')
+NON_DATA_PARAMS = (GCM_RECEIVE_KEY_NAME, 'repunch_receivers')
 GCM_URL = "https://android.googleapis.com/gcm/send"
+
+RECEIVER_BATCH_SIZE = 200
 
 def gcm_send(postBody):
     """
@@ -17,8 +19,10 @@ def gcm_send(postBody):
         {
             # Required content
             gcmrkey: "<<GCM_RECEIVE_KEY>>",
-            registration_ids: ["kh7x7towm4t...", "uytxgygx....", ...],
-            employee_ids or patron_ids: ["SGDgsas", ...],
+            repunch_receivers: [{
+                "registration_id": "..."
+                "<employee|patron>_id": "...",
+            }, ...],
 	        action: "com.repunch.retailer.PUNCH",
         
             # optional content (must be less than 4kb)
@@ -31,29 +35,52 @@ def gcm_send(postBody):
         
     See http://developer.android.com/google/gcm/server.html#params
     for more GCM parameters.
+    
+    Note that Google GCM Servers convert all data to string.
     """
     if str(postBody[GCM_RECEIVE_KEY_NAME]) != GCM_RECEIVE_KEY:
         return False
     
+    receivers = postBody['repunch_receivers']
+    if len(receivers) == 0:
+        return True
+        
     headers = {
         'UserAgent': "GCM-Server",
         'content-type': 'application/json',
         'Authorization': 'key=%s' % (GCM_API_KEY,),
     }
-    payload = {
-        "registration_ids": postBody['registration_ids'],
-        "data": { k:v for k,v in postBody.iteritems()
-            if k not in NON_DATA_PARAMS },
-    }
     
-    res = requests.post(GCM_URL, data=json.dumps(payload),
-        headers=headers)
-    
-    # TODO make sure the employee_ids | patron_ids do not end up
-    # exceeding the 4kb limit of a GCM push
-    
-    # TODO handle response
-    print res.json()
+    data = { k:v for k,v in postBody.iteritems()
+        if k not in NON_DATA_PARAMS }
     
     
-    return res.ok
+    # its either all employee or patron
+    if "employee_id" in receivers[0].keys():
+        which_ids = "employee_ids"
+    else:
+        which_ids = "patron_ids"
+        
+    # chunk messages to make sure the employee_ids | patron_ids
+    # do not end up exceeding the 4kb limit of a GCM push
+    while len(receivers) > 0:
+        registration_ids, extra_ids = {}, [], []
+        rec = receivers[:RECEIVER_BATCH_SIZE]
+        receivers = rec
+        for r in rec:
+            registration_ids.append(r["registration_id"])
+            extra_ids.append(r[which_id])
+            
+        batch_data = data.copy()
+        batch_data.update({which_ids: extra_ids})
+    
+        res = requests.post(GCM_URL, data=json.dumps({
+            "registration_ids": registration_ids,
+            "data": batch_data,
+        }), headers=headers)
+        
+        # TODO handle response
+        print res.json()
+    
+    
+    return True
