213fc24939fae801652aacf5298213d49e9f829d
vestrel00 <vestrel00@gmail.com>
2013-10-28 14:27:56 -0400

    Changed emplkoyee delete action.

--- cloud_code/master/cloud/main.js
+++ cloud_code/master/cloud/main.js
@@ -522,7 +522,7 @@ Parse.Cloud.define("delete_employee", function(request, response)
                 body: {
                     gcmrkey: "<<GCM_RECEIVE_KEY>>",
                     repunch_receivers: repunchReceivers, 
-			        action: "com.repunch.retailer.EMPLOYEE_DELETE",
+			        action: "com.repunch.retailer.EMPLOYEE_DELETED",
                 }, 
                 success: function(httpResponse) {
                     console.log("Post success with " + httpResponse.text);
