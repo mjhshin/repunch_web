65dd5a91ee036c498ca3bbcc2b3a8274bdb0e0d9
vestrel00 <vestrel00@gmail.com>
2013-11-21 14:33:24 -0500

    Added CloudCodeTests and prepared add/delete patronstore tests.

--- cloud_code/tests/__init__.py
+++ cloud_code/tests/__init__.py
@@ -1,1 +1,34 @@
+"""
+Utility methods for cloud_code tests.
 
+These should also be used for SeleniumTests.
+"""
+
+class CloudCodeTest(object):
+    
+    def __init__(self, test_title, tests):
+        """
+        tests has the following format:
+        [ {'test_name': "Test title"}, ... ]
+        """
+        self._results = []
+        section = {
+            "section_name": test_title,
+            "parts": tests,
+        }
+        self._results.append(section)
+        self.tests = tests
+    
+    def get_results(self):
+        return self._results
+    
+    def testit(self, test_num, test):
+        """
+        test is a function that must return a bool.
+        """
+        try:
+            self.tests[test_num]["success"] = test()
+        except Exception as e:
+            print "Error: %s\n%s" %\
+                (self.tests[test_num]["test_name"], e)
+            self.tests[test_num]["test_message"] = str(e)
--- cloud_code/tests/test_add_delete_patronstore.py
+++ cloud_code/tests/test_add_delete_patronstore.py
@@ -3,6 +3,7 @@ Tests for add_patronstore and delete_patronstore.
 """
 
 from parse.utils import cloud_call
+from cloud_code import CloudCodeTest
 
 # The patron store created from the below are temporary.
 # This means it gets deleted.
@@ -13,20 +14,77 @@ def test_add_delete_patron_store():
     """
     This test also calls punch.
     """
-    results = []
-    parts = [
-        {'test_name': "Non-existent PatronStore is created"},
+    test = CloudCodeTest("ADD_DELETE_PATRONSTORE", [
+        {'test_name': "Add_patronstore creates a new PatronStore if does not exist"},
         {'test_name': "PatronStore is added to Patron's PatronStores relation"},
         {'test_name': "PatronStore is added to Store's PatronStores relation"},
-    ]
-    section = {
-        "section_name": "ADD_PATRONSTORE",
-        "parts": parts,
-    }
-    results.append(section)
+        {'test_name': "PatronStore's Patron pointer is set"},
+        {'test_name': "PatronStore's Store pointer is set"},
+        {'test_name': "PatronStore's all_time_punches is set to 0"},
+        {'test_name': "PatronStore's punch_count is set to 0"},
+        {'test_name': "PatronStore's pending_reward is set to false"},
+        
+        {'test_name': "Add_patronstore returns existing PatronStore"},
+        {'test_name': "PatronStore is in the Patron's PatronStores relation"},
+        {'test_name': "PatronStore is in the Store's PatronStores relation"},
+        
+        {'test_name': "Delete_patronstore deletes the PatronStore"},
+        {'test_name': "PatronStore is removed from the Patron's PatronStores relation"},
+        {'test_name': "PatronStore is removed from the Store's PatronStores relation"},
+        ###
+        {'test_name': "Punch creates a new PatronStore if does not exist"},
+        {'test_name': "PatronStore is added to Patron's PatronStores relation"},
+        {'test_name': "PatronStore is added to Store's PatronStores relation"},
+        {'test_name': "PatronStore's Patron pointer is set"},
+        {'test_name': "PatronStore's Store pointer is set"},
+        {'test_name': "PatronStore's all_time_punches is set to 0"},
+        {'test_name': "PatronStore's punch_count is set to 0"},
+        {'test_name': "PatronStore's pending_reward is set to false"},
+        
+        {'test_name': "Add_patronstore returns existing PatronStore"},
+        {'test_name': "PatronStore is in the Patron's PatronStores relation"},
+        {'test_name': "PatronStore is in the Store's PatronStores relation"},
+        
+        {'test_name': "Delete_patronstore deletes the PatronStore"},
+        {'test_name': "PatronStore is removed from the Patron's PatronStores relation"},
+        {'test_name': "PatronStore is removed from the Store's PatronStores relation"},
+    ])
     
+    ##########  Add_patronstore creates a new PatronStore if does not exist TODO
+    ##########  PatronStore is added to Patron's PatronStores relation TODO
+    ##########  PatronStore is added to Store's PatronStores relation TODO
+    ##########  PatronStore's Patron pointer is set TODO
+    ##########  PatronStore's Store pointer is set TODO
+    ##########  PatronStore's all_time_punches is set to 0 TODO
+    ##########  PatronStore's punch_count is set to 0 TODO
+    ##########  PatronStore's pending_reward is set to false TODO
+        
+    ##########  Add_patronstore returns existing PatronStore TODO
+    ##########  PatronStore is in the Patron's PatronStores relation TODO
+    ##########  PatronStore is in the Store's PatronStores relation TODO
+        
+    ##########  Delete_patronstore deletes the PatronStore TODO
+    ##########  PatronStore is removed from the Patron's PatronStores relation TODO
+    ##########  PatronStore is removed from the Store's PatronStores relation TODO
     
-    
+    ###
+        
+    ##########  Punch creates a new PatronStore if does not exist TODO
+    ##########  PatronStore is added to Patron's PatronStores relation TODO
+    ##########  PatronStore is added to Store's PatronStores relation TODO
+    ##########  PatronStore's Patron pointer is set TODO
+    ##########  PatronStore's Store pointer is set TODO
+    ##########  PatronStore's all_time_punches is set to 0 TODO
+    ##########  PatronStore's punch_count is set to 0 TODO
+    ##########  PatronStore's pending_reward is set to false TODO
+        
+    ##########  Add_patronstore returns existing PatronStore TODO
+    ##########  PatronStore is in the Patron's PatronStores relation TODO
+    ##########  PatronStore is in the Store's PatronStores relation TODO
+        
+    ##########  Delete_patronstore deletes the PatronStore TODO
+    ##########  PatronStore is removed from the Patron's PatronStores relation TODO
+    ##########  PatronStore is removed from the Store's PatronStores relation TODO
     
     # END OF ALL TESTS - cleanup
     return results
