d46950c070912d38e19cdc4e63dfae1c57105584
vestrel00 <vestrel00@gmail.com>
2013-11-22 11:06:56 -0500

    Prepared tests for punch.

--- cloud_code/tests/test_punch.py
+++ cloud_code/tests/test_punch.py
@@ -26,5 +26,58 @@ def test_punch():
     store.patronStores = None
     
     test = CloudCodeTest("PUNCH", [
-        {'test_name': ""},
+        {'test_name': "Punch creates a new Punch object (from employee)"},
+        {'test_name': "Punch object is added to Store's Punches relation"},
+        {'test_name': "Punch object is added to Employee's Punches relation"},
+        {'test_name': "Patron pointer is set"},
+        {'test_name': "Punches is set to correct amount"},
+        {'test_name': "Employee's life_time_punches is updated"},
+        {'test_name': "PatronStore is created since it does not yet exist"},
+        {'test_name': "PatronStore is added to Patron's PatronStores relation"},
+        {'test_name': "PatronStore is added to Store's PatronStores relation"},
+        {'test_name': "PatronStore's Patron pointer is set"},
+        {'test_name': "PatronStore's Store pointer is set"},
+        {'test_name': "PatronStore's all_time_punches is set to 0"},
+        {'test_name': "PatronStore's punch_count is set to 0"},
+        {'test_name': "PatronStore's pending_reward is set to false"},
+        
+        {'test_name': "Punch creates a new Punch object (from dashboard - owner)"},
+        {'test_name': "Punch object is added to Store's Punches relation"},
+        {'test_name': "Patron pointer is set"},
+        {'test_name': "Punches is set to correct amount"},
+        {'test_name': "PatronStore's punch_count is updated"},
+        {'test_name': "PatronStore's all_time_punches is updated"},
+        
+        {'test_name': "Using unused punch_code returns error with PATRON_NOT_FOUND"},
     ])
+    
+    
+    ##########  Punch creates a new Punch object (from employee) TODO
+    def test_0():
+        return True # TODO
+    
+    test.testit(test_0)
+    
+    ##########  Punch object is added to Store's Punches relation TODO
+    ##########  Punch object is added to Employee's Punches relation TODO
+    ##########  Patron pointer is set TODO
+    ##########  Punches is set to correct amount TODO
+    ##########  Employee's life_time_punches is updated TODO
+    ##########  PatronStore is created since it does not yet exist TODO
+    ##########  PatronStore is added to Patron's PatronStores relation TODO
+    ##########  PatronStore is added to Store's PatronStores relation TODO
+    ##########  PatronStore's Patron pointer is set TODO
+    ##########  PatronStore's Store pointer is set TODO
+    ##########  PatronStore's all_time_punches is set to 0 TODO
+    ##########  PatronStore's punch_count is set to 0 TODO
+    ##########  PatronStore's pending_reward is set to false TODO
+        
+    ##########  Punch creates a new Punch object (from dashboard - owner) TODO
+    ##########  Punch object is added to Store's Punches relation TODO
+    ##########  Patron pointer is set TODO
+    ##########  Punches is set to correct amount TODO
+    ##########  PatronStore's punch_count is updated TODO
+    ##########  PatronStore's all_time_punches is updated TODO
+        
+    ##########  Using unused punch_code returns error with PATRON_NOT_FOUND TODO
+    
